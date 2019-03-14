4eca8a8cb2e4a2b9a985dded646fecb08edadd9c
harshithaRao5 <harshithanallani@msitprogram.net>
2019-03-14 17:06:12 +0530

    m3

--- Module 3/Activity 29/Exercise-PolishAsyncTask/T05b.03-Exercise-PolishAsyncTask/app/src/main/java/com/example/android/asynctaskloader/MainActivity.java
+++ Module 3/Activity 29/Exercise-PolishAsyncTask/T05b.03-Exercise-PolishAsyncTask/app/src/main/java/com/example/android/asynctaskloader/MainActivity.java
@@ -165,8 +165,8 @@ public class MainActivity extends AppCompatActivity implements
     public Loader<String> onCreateLoader(int id, final Bundle args) {
         return new AsyncTaskLoader<String>(this) {
 
-            // TODO (1) Create a String member variable called mGithubJson that will store the raw JSON
-
+            // (1) Create a String member variable called mGithubJson that will store the raw JSON
+            String mGithubJson;
             @Override
             protected void onStartLoading() {
 
@@ -175,8 +175,12 @@ public class MainActivity extends AppCompatActivity implements
                     return;
                 }
 
-                // TODO (2) If mGithubJson is not null, deliver that result. Otherwise, force a load
-
+                // (2) If mGithubJson is not null, deliver that result. Otherwise, force a load
+                if(mGithubJson!=null){
+                    deliverResult(mGithubJson);
+                }else{
+                    forceLoad();
+                }
                 /*
                  * When we initially begin loading in the background, we want to display the
                  * loading indicator to the user
@@ -208,8 +212,13 @@ public class MainActivity extends AppCompatActivity implements
                 }
             }
 
-            // TODO (3) Override deliverResult and store the data in mGithubJson
-            // TODO (4) Call super.deliverResult after storing the data
+            // (3) Override deliverResult and store the data in mGithubJson
+            @Override
+            public void deliverResult(String githubJSON){
+                mGithubJson = githubJSON;
+                super.deliverResult(githubJSON);
+            }
+            //  (4) Call super.deliverResult after storing the data
         };
     }
 
